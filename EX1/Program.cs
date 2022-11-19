6df3dca5e53c9a387e48e5add6dc920e0f42a704
LEN4 <fooggreitan@gmail.com>
2022-11-20 00:36:40 +0300

    new1

--- EX1/Program.cs
+++ EX1/Program.cs
@@ -1,36 +1,68 @@
-﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-// m = 3, n = 4.
-// 0,5 7 -2 -0,2
-// 1 -3,3 8 -9,9
-// 8 7,8 -7,1 9
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
 
-double[,] CreateArrayRndInt(int row, int columns, double min, double max)
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
 {
-    var array = new double[row, columns];
+    var matrix = new int[rows, columns];
     var rnd = new Random();
-    for (int i = 0; i < array.GetLength(0); i++)
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            array[i, j] = Math.Round(rnd.NextDouble(), 2);
+            matrix[i, j] = rnd.Next(min, max + 1);
         }
     }
-    return array;
+    return matrix;
 }
 
-void PrintArray(double[,] array)
-{  
-    for (int i = 0; i < array.GetLength(0); i++)
+void ChangeMatrix(int[,] matrix)
+{
+    int temp = 0;
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        Console.Write("[");
-        for (int j = 0; j < array.GetLength(1); j++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            if (j < array.GetLength(1) - 1) Console.Write($"{array[i,j], 6}, "); // форматирование
-            else Console.Write($"{array[i,j], 6}"); // форматирование
+            for (int m = 0; m < matrix.GetLength(1) - 1; m++)
+            {
+                if (matrix[i, m] < matrix[i, m + 1])
+            {
+                temp = matrix[i, m];
+                matrix[i, m] = matrix[i, m + 1];
+                matrix[i, m + 1] = temp;
+            }
+            }
         }
-        Console.WriteLine("]");
     }
 }
 
-double[,] array2D = CreateArrayRndInt(3, 4, 1, 100);
-PrintArray(array2D);
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5}, ");
+            else Console.Write($"{matrix[i, j],5} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[,] array2D = CreateMatrixRndInt(3, 3, 1, 10);
+PrintMatrix(array2D);
+
+Console.WriteLine();
+Console.WriteLine("Отсортированный массив по убыванию: ");
+
+ChangeMatrix(array2D);
+PrintMatrix(array2D);
