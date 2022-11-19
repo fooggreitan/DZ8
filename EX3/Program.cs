b7b8dfa66c4d491d8405a2d411713cc844a2b427
LEN4 <fooggreitan@gmail.com>
2022-11-20 00:36:54 +0300

    new1

--- EX3/Program.cs
+++ EX3/Program.cs
@@ -1,59 +1,111 @@
-﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
-// Например, задан массив:
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
-// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
 
-int[,] CreateArrayRndInt(int row, int columns, int min, int max)
+Console.WriteLine("Введите размерноcть первой матрицы: ");
+Console.Write("m = ");
+int numberOneMatrixOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("k = ");
+int numberTwoMatrixOne = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите размерноcть второй матрицы: ");
+Console.Write("k = ");
+int numberOneMatrixTwo = Convert.ToInt32(Console.ReadLine());
+Console.Write("n = ");
+int numberTwoMatrixTwo = Convert.ToInt32(Console.ReadLine());
+
+int[,] CreateMatrixRndIntFist(int rowss, int columnss)
 {
-    var array = new int[row, columns];
+    var matrixOne = new int[rowss, columnss];
     var rnd = new Random();
-    for (int i = 0; i < array.GetLength(0); i++)
+
+    for (int i = 0; i < matrixOne.GetLength(0); i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
+        for (int j = 0; j < matrixOne.GetLength(1); j++)
         {
-            array[i, j] = rnd.Next(min, max + 1);
+            matrixOne[i, j] = rnd.Next(10);
         }
     }
-    return array;
+    return matrixOne;
 }
 
-double[] ArithmeticMeanValues(int[,] array)
+int[,] CreateMatrixRndIntSecond(int rows, int columns)
 {
-    double[] amv = new double[array.GetLength(1)];
-    
-    for (int j = 0; j < array.GetLength(1); j++)
+    var matrixTwo = new int[rows, columns];
+    var rnd = new Random();
+
+    for (int i = 0; i < matrixTwo.GetLength(0); i++)
     {
-        double sum = default;
-
-        for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < matrixTwo.GetLength(1); j++)
         {
-            sum += array[i,j];
+            matrixTwo[i, j] = rnd.Next(10);
         }
-
-        amv[j] = Math.Round(sum / array.GetLength(0), 1);
     }
-
-    return amv;
+    return matrixTwo;
 }
 
-void PrintArray(int[,] array)
+bool ExaminationMatrix(int[,] matrixOne, int[,] matrixTwo)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+    if ((matrixOne.GetLength(0) == matrixTwo.GetLength(1)) && (matrixOne.GetLength(1) == matrixTwo.GetLength(0))) return true;
+    else return false;
+}
+
+int[,] CompositionMatrix(int[,] matrixOne, int[,] matrixTwo)
+{
+    var compositionMatrix = new int[matrixOne.GetLength(0), matrixTwo.GetLength(1)];
+
+    for (int i = 0; i < matrixOne.GetLength(0); i++)
     {
-        Console.Write("[");
-        for (int j = 0; j < array.GetLength(1); j++)
+        for (int j = 0; j < matrixTwo.GetLength(1); j++)
         {
-            if (j < array.GetLength(1) - 1) Console.Write($"{array[i, j],6}, "); // форматирование
-            else Console.Write($"{array[i, j],6}"); // форматирование
+            compositionMatrix[i, j] = 0;
+
+            for (int k = 0; k < matrixOne.GetLength(1); k++)
+            {
+                compositionMatrix[i, j] += matrixOne[i, k] * matrixTwo[k, j];
+            }
         }
-        Console.WriteLine("]");
+    }
+    return compositionMatrix;
+}
+
+
+void PrintMatrixR(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5}");
+            else Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine("|");
     }
 }
 
-int[,] array2D = CreateArrayRndInt(3, 4, 1, 100);
-PrintArray(array2D);
+Console.WriteLine("");
 
-double[] avg = ArithmeticMeanValues(array2D);
-Console.WriteLine($"{avg[0],6}; {avg[1],6}; {avg[2],6}; {avg[3],6}");
+int[,] matrixOne = CreateMatrixRndIntFist(numberOneMatrixOne, numberTwoMatrixOne);
+PrintMatrixR(matrixOne);
+
+Console.WriteLine("");
+
+int[,] matrixTwo = CreateMatrixRndIntSecond(numberOneMatrixTwo, numberTwoMatrixTwo);
+PrintMatrixR(matrixTwo);
+
+Console.WriteLine("");
+
+bool examinationMatrix = ExaminationMatrix(matrixOne, matrixTwo);
+if (examinationMatrix)
+{
+    Console.WriteLine("Произведение матриц двух матриц:");
+
+    int[,] compositionMatrix = CompositionMatrix(matrixTwo, matrixTwo);
+    PrintMatrixR(compositionMatrix);
+}
+else Console.WriteLine("Невозможно перемножить матрицы!");
